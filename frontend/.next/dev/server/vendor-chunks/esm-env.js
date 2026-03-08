bfb8e6ad04fd62ae956ac5cefd7d21f46bdd567e
sakemyali <emoulayali54@gmail.com>
2026-03-08 18:19:15 +0900

    docs: add comprehensive README and rebrand to Maisen
    - Rebrand project from "UX Feedback Compiler" to "Maisen" across all files
    - Update package.json names (root and server packages)
    - Update app title in Header.tsx and index.html
    - Add comprehensive README with Bahja-style documentation including:
      - Problem/Solution framework
      - Architecture diagram with ASCII art
      - Complete API reference with examples
      - Detailed project structure and tech stack
      - Implementation details for AI pipeline
      - Deployment instructions
      - Example usage and test prompts

---
+++ frontend/.next/dev/server/vendor-chunks/esm-env.js
@@ -0,0 +1,45 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/esm-env";
+exports.ids = ["vendor-chunks/esm-env"];
+exports.modules = {
+
+/***/ "(ssr)/./node_modules/esm-env/false.js":
+/*!***************************************!*\
+  !*** ./node_modules/esm-env/false.js ***!
+  \***************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (false);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvZXNtLWVudi9mYWxzZS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsS0FBSyxFQUFDIiwic291cmNlcyI6WyIvVXNlcnMvbXZybS9EZXNrdG9wL0hhY2thdGhvbnMvYzBtcGlsZWQtVG9reW8yMDI2L2Zyb250ZW5kL25vZGVfbW9kdWxlcy9lc20tZW52L2ZhbHNlLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZhbHNlO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/esm-env/false.js\n");
+
+/***/ }),
+
+/***/ "(ssr)/./node_modules/esm-env/index.js":
+/*!***************************************!*\
+  !*** ./node_modules/esm-env/index.js ***!
+  \***************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   BROWSER: () => (/* reexport safe */ esm_env_browser__WEBPACK_IMPORTED_MODULE_0__[\"default\"]),\n/* harmony export */   DEV: () => (/* reexport safe */ esm_env_development__WEBPACK_IMPORTED_MODULE_1__[\"default\"]),\n/* harmony export */   NODE: () => (/* reexport safe */ esm_env_development__WEBPACK_IMPORTED_MODULE_1__[\"default\"])\n/* harmony export */ });\n/* harmony import */ var esm_env_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! esm-env/browser */ \"(ssr)/./node_modules/esm-env/false.js\");\n/* harmony import */ var esm_env_development__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! esm-env/node */ \"(ssr)/./node_modules/esm-env/true.js\");\n\n\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvZXNtLWVudi9pbmRleC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFxRDtBQUNBO0FBQ04iLCJzb3VyY2VzIjpbIi9Vc2Vycy9tdnJtL0Rlc2t0b3AvSGFja2F0aG9ucy9jMG1waWxlZC1Ub2t5bzIwMjYvZnJvbnRlbmQvbm9kZV9tb2R1bGVzL2VzbS1lbnYvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IHsgZGVmYXVsdCBhcyBCUk9XU0VSIH0gZnJvbSAnZXNtLWVudi9icm93c2VyJztcbmV4cG9ydCB7IGRlZmF1bHQgYXMgREVWIH0gZnJvbSAnZXNtLWVudi9kZXZlbG9wbWVudCc7XG5leHBvcnQgeyBkZWZhdWx0IGFzIE5PREUgfSBmcm9tICdlc20tZW52L25vZGUnO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/esm-env/index.js\n");
+
+/***/ }),
+
+/***/ "(ssr)/./node_modules/esm-env/true.js":
+/*!**************************************!*\
+  !*** ./node_modules/esm-env/true.js ***!
+  \**************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (true);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHNzcikvLi9ub2RlX21vZHVsZXMvZXNtLWVudi90cnVlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpRUFBZSxJQUFJLEVBQUMiLCJzb3VyY2VzIjpbIi9Vc2Vycy9tdnJtL0Rlc2t0b3AvSGFja2F0aG9ucy9jMG1waWxlZC1Ub2t5bzIwMjYvZnJvbnRlbmQvbm9kZV9tb2R1bGVzL2VzbS1lbnYvdHJ1ZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB0cnVlO1xuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(ssr)/./node_modules/esm-env/true.js\n");
+
+/***/ })
+
+};
+;
